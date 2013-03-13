5e5576f2f60cfdb72ebe00e458d971aac6e90e00
Rustam Azimov <rustam.azimov19021995@gmail.com>
2013-03-14 01:31:55 +0300

    This version is ready to test. Add doxygen comments.

--- secondSemester/hw4/hw4Num1/main.cpp
+++ secondSemester/hw4/hw4Num1/main.cpp
@@ -1,6 +1,15 @@
 #include <QtGui/QApplication>
 #include "widget.h"
 
+ /**
+ * @file main.cpp
+ * @author Azimov Rustam <rustam.azimov19021995@gmail.com>
+ *
+ * @section DESCRIPTION
+ *
+ * In this program you can play all night long...
+ */
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
